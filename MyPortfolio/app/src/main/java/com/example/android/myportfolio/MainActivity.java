3b61adbd2c7b289f6fb29de8e40bdf663f920f69
Marina Borges <marina@3des.com.br>
2017-05-22 11:44:42 -0300

    Change the button method, to show what button was clicked

--- MyPortfolio/app/src/main/java/com/example/android/myportfolio/MainActivity.java
+++ MyPortfolio/app/src/main/java/com/example/android/myportfolio/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.myportfolio;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //I used this method when I lerned the project called Just Java in a free Udacity course;
-    public void buttonMessage (View view){
-        Toast.makeText(this, "This button will launch my capstone project", Toast.LENGTH_SHORT).show();
+    //public void buttonMessage (View view){
+    //    Toast.makeText(this, "This button will launch my capstone project", Toast.LENGTH_SHORT).show();
+    //}
+
+
+    public void sendMessage(View view) {
+        Context context = view.getContext();
+        int duration = Toast.LENGTH_SHORT;
+        Button button = (Button) view; //O nome da aplicação está configurada no 'text' do botão
+        String message = String.format(getString(R.string.toast_message), button.getText().toString().toLowerCase());
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
     }
 }
